31038b9e9b861927ce872b380c054a2349265faa
David Prelogovic <david.prelogovic@gmail.com>
2023-05-04 16:06:00 +0200

    add cond classes to import

---
+++ py21cmlikelihoods/__init__.py
@@ -0,0 +1,8 @@
+from .gauss import ConditionalGaussian, ConditionalGaussianMixture
+from .maf import ConditionalMaskedAutoregressiveFlow
+
+__all__ = [
+    "ConditionalGaussian",
+    "ConditionalGaussianMixture",
+    "ConditionalMaskedAutoregressiveFlow",
+]
